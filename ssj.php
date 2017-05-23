734ddfc8583803618ff8a7103a731b9904dd4ab1
Osamu Nagayama <osanaga@gmail.com>
2017-05-23 11:14:25 +0900

    ver 1.0

--- ssj.php
+++ ssj.php
@@ -1,6 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Seiteki Saito Jenereta</title>
+</head>
+<body>
 <?php
-// Seiteki Saito Jenereetaa
-
 // 反映元
 $src = 'index.html';
 
@@ -13,17 +18,35 @@ $sections = ['header', 'sidebar', 'footer'];
 $parts = [];
 $html = file_get_contents($src);
 foreach ($sections as $section) {
-    preg_match('|<!-- {' . $section . '} -->(.*?)<!-- /{' . $section . '} -->|s', $html, $ms);
+    preg_match('|<!-- <' . $section . '> -->(.*?)<!-- /<' . $section . '> -->|s', $html, $ms);
     $parts[$section] = $ms[1];
 }
-preg_match_all('|<!-- {s(\d+?)} -->(.*?)<!-- /{s\d+?} -->|s', $html, $mss, PREG_SET_ORDER);
+preg_match_all('|<!-- <slot(\d+?)> -->.*?<!-- /<slot\d+?> -->|s', $html, $mss, PREG_SET_ORDER);
 $slots = [];
-foreach ($mss as $ms) $slots[$ms[1]] = $ms[2];
+foreach ($mss as $ms) $slots[$ms[1]] = null;
 foreach ($dists as $dist) {
+    echo "$dist:<br>";
     $html = file_get_contents($dist);
+    foreach ($slots as $no => $slot) {
+        if (preg_match('|<!-- <slot' . $no . '> -->(.*?)<!-- /<slot' . $no . '> -->|s', $html, $ms)) {
+            $slots[$no] = $ms[1];
+        } else {
+            $slots[$no] = null;
+        }
+    }
     foreach ($parts as $section => $part) {
-        $html = preg_replace('|(<!-- {' . $section . '} -->)(.*?)(<!-- /{' . $section . '} -->)|s', "$1$part$3", $html);
+        $html = preg_replace('|(<!-- <' . $section . '> -->).*?(<!-- /<' . $section . '> -->)|s', '${1}' . $part . '${2}', $html);
+        echo " section: $section OK<br>";
+    }
+    foreach ($slots as $no => $slot) {
+        if ($slot) {
+            $html = preg_replace('|(<!-- <slot' . $no . '> -->).*?(<!-- /<slot' . $no . '> -->)|s', '${1}' . $slot . '${2}', $html);
+            echo " slot$no: $slot OK<br>";
+        }
     }
     file_put_contents($dist, $html);
-    echo "$dist OK<br>";
+    echo '<br>';
 }
+?>
+</body>
+</html>
